43bd952dd49c8d375c00538bbc10aedb5ef0597b
PRUDHVI-9 <prudhvirajathelli@gmail.com>
2023-04-08 12:37:59 +0530

    Add:calculating wages for a month.

---
+++ employeeWages/WagesForMonth.java
@@ -0,0 +1,52 @@
+package employeeWages;
+
+public class WagesForMonth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Declaring a variable for switch expression .
+		 * Calculate the random number.
+		 * Switch expression.
+		 * 
+		 */
+
+		// Declaring a variable for switch expression
+		int wageOfEmployee = 20;
+		int sum = 0;
+		int wage = 0;
+		int FullDayHour = 8;
+		int PartTimeHour = 8;
+
+		for (int day = 1; day <= 20; day++) {
+
+			/*
+			 * Calculate the random number
+			 */
+			int isPresent = (int) (Math.random() * 3);
+
+			// Switch expression
+			switch (isPresent) {
+			// Case statements
+			case 1:
+				System.out.println("Employee is present");
+				wage = FullDayHour * wageOfEmployee;
+				sum = sum + wage;
+				System.out.println("Employee earned " + sum);
+				break;
+
+			case 2:
+				System.out.println("Employee is partial present");
+				wage = PartTimeHour * wageOfEmployee;
+				sum = sum + wage;
+				System.out.println("Employee earned " + sum);
+				break;
+
+			// Default case statement
+			default:
+				System.out.println("Employee is absent");
+
+			}
+		}
+	}
+}
